1a3f48baac4cc61db803f872fb15fc4945ffdc4c
Le Minh Triet <s4112882@rmit.edu.vn>
2026-03-13 21:51:55 +1100

    I forgot to import env :<

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values 
+# loading variables from .env file
+load_dotenv() 
 from google import genai
 
-client = genai.Client(api_key="YOUR_API_KEY")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
